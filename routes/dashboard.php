00594f4b2c9324dc95d2cd93c78fb1eed41d6941
alvian <alvian.ardhiansyah@gmail.com>
2020-01-12 01:14:59 +0700

    revisi

---
+++ routes/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::group(['prefix' => 'admin'], function () {
+
+Route::get('/data','DashboardController@index')->name('data');
+Route::get('/perhitungan','DepositController@index')->name('perhitungan');
+Route::post('/perhitungan/fetch','DepositController@fetch');
+Route::post('/perhitungan/fetchbunga','DepositController@fetchbunga');
+
+
+});
