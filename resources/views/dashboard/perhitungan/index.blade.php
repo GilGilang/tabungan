b2a26d263517764cbb85314b687a16841e18f568
alvian <alvian.ardhiansyah@gmail.com>
2020-01-12 12:19:07 +0700

    revisi

--- resources/views/dashboard/perhitungan/index.blade.php
+++ resources/views/dashboard/perhitungan/index.blade.php
@@ -46,7 +46,7 @@
                 </form>
               </div>
               <div class="card-footer">
-                <button type="submit" class="btn btn-fill btn-primary save">Save</button>
+                <button type="submit" class="btn btn-fill btn-primary save">Hitung</button>
               </div>
             </div>
           </div>
@@ -57,27 +57,74 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+          <h3 class="col-12 modal-title text-center" id="exampleModalLabel">Deposit</h3>
+
         </div>
         <div class="modal-body">
-          ...
+         <form action="">
+
+            <table class="profit-loss report-table table" id="date-profit-lost">
+
+                <tbody >
+                    <tr>
+                        <td class="report-subtotal text-left regular-text data-col-half" colspan="2">
+                            <p class="text-dark ">Nama Bank</p>
+                          </td>
+                          <td class="report-subtotal text-right" id="assets-type-1-total-data">
+                          </td>
+                          <td class="border-top-thin"  style="padding-left:80px;">
+                            <p class="text-dark" id="namabank"> </p>
+                          </td>
+                   </tr>
+                        <tr >
+                              <td  class="report-subtotal text-left regular-text data-col-half" colspan="2">
+                                <p class="text-dark ">Bunga + Saldo</p>
+                                </td>
+                                <td class="report-subtotal text-right" id="assets-type-1-total-data">
+                                </td>
+                                <td class="border-top-thin" name="hasilnama" style="padding-left:80px;">
+                                    <p class="text-dark" id="bungadansaldo"> </p>
+                                </td>
+                         </tr>
+                           <tr>
+                                <td class="report-subtotal text-left regular-text data-col-half" colspan="2">
+                                    <p class="text-dark ">Saldo</p>
+                                  </td>
+                                  <td class="report-subtotal text-right" id="assets-type-1-total-data">
+                                  </td>
+                                  <td class="border-top-thin"  style="padding-left:80px;">
+                                    <p class="text-dark" id="saldosaja"> </p>
+                                  </td>
+                           </tr>
+                           <tr>
+                                <td class="report-subtotal text-left regular-text data-col-half" colspan="2">
+                                    <p class="text-dark ">Bunga</p>
+                                  </td>
+                                  <td class="report-subtotal text-right" id="assets-type-1-total-data">
+                                  </td>
+                                  <td class="border-top-thin"  style="padding-left:80px;">
+                                    <p class="text-dark " id="bungasaja"></p>
+                                  </td>
+                           </tr>
+
+
+                </tbody>
+            </table>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
           <button type="button" class="btn btn-primary">Save changes</button>
         </div>
+    </form>
       </div>
     </div>
 </div>
 
 @endsection
 @section('script')
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="{{asset('assets/js/sweetalert.min.js')}}"></script>
 <script src="{{asset('assets/js/jquery.lwMultiSelect.js')}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
+<script src="{{asset('assets/js/jquery.mask.js')}}"></script>
 <script>
 $(document).ready(function(){
     $('#bank').on('change', function(){
@@ -113,6 +160,29 @@ $(document).ready(function(){
 
     $('.save').on('click', function(){
         $('#exampleModal').modal('show');
+        var jumlah = $('#jumlah').val();
+        var bank = $('#bank').val();
+        var waktu = $('#waktu').val();
+        var bunga = $('#bunga').val();
+
+        ajax();
+        $.ajax({
+            url:'/admin/perhitungan/deposit',
+            method:'POST',
+            data: {
+                    'modal':1,
+                    'jumlah':jumlah,
+                    'bank':bank,
+                    'waktu':waktu,
+                    'bunga':bunga
+
+            }, success:function(response){
+               $('#namabank').text(response['bank']);
+               $('#bungadansaldo').text(response['saldobunga']);
+               $('#saldosaja').text(response['jumlah']);
+               $('#bungasaja').text(response['hasil']);
+            }
+        });
     });
 
 });
