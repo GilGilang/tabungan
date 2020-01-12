a75869d9308094eae49daf3e0f2e35eebce3a595
alvian <alvian.ardhiansyah@gmail.com>
2020-01-12 12:19:16 +0700

    revisi

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -6,6 +6,6 @@ Route::get('/data','DashboardController@index')->name('data');
 Route::get('/perhitungan','DepositController@index')->name('perhitungan');
 Route::post('/perhitungan/fetch','DepositController@fetch');
 Route::post('/perhitungan/fetchbunga','DepositController@fetchbunga');
-
+Route::post('/perhitungan/deposit','DepositController@deposithitung');
 
 });
